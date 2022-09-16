ef8c9a1ed69082d2171ba8e20e975bc1271710a7
Yurii Dereguzov <DereguzovYE@yandex.ru>
2022-09-16 15:40:40 +0500

    feat: Добавить элументы с помощью template

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,3 @@
-const popup = document.querySelector('.popup');
-const openPopupButton = document.querySelector('.profile__edit-button');
-const closePopupButton = popup.querySelector('.popup__close-button');
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.profile__name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.profile__job');// Воспользуйтесь инструментом .querySelector()
-let newTextName = document.querySelector('.popup__input_type_name')
-let newTextJob = document.querySelector('.popup__input_type_job')
-
-
-const togglePopup = () => {
-    popup.classList.toggle('popup_opened');
-}
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-    nameInput.textContent = newTextName.value;
-    jobInput.textContent = newTextJob.value;
-    togglePopup();
-}
-
-
-openPopupButton.addEventListener('click', () => {
-    togglePopup();
-    newTextName.value = nameInput.textContent;
-    newTextJob.value = jobInput.textContent;
-})
-
-closePopupButton.addEventListener('click', togglePopup);
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
-
-
 // openPopupButton.addEventListener('click', () => {
 //     overlayEl.classList.add('overlay_opened');
 // })
@@ -67,13 +19,197 @@ formElement.addEventListener('submit', formSubmitHandler);
 // });
 
 
+// const cardElement = document.querySelector('.card');
+// cardElement.querySelector('.card__like').addEventListener('click', (e) => {
+//     e.target.classList.toggle('card__like_active');
+//   });
 
-// const likes = document.querySelectorAll('.card__like');
 
-// const toggleClass = (e) => {
-//     e.classList.toggle('card__like_active');
+
+const popup = document.querySelector('.popup');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closePopupButton = popup.querySelector('.popup__close-button');
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.profile__name');// Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.profile__job');// Воспользуйтесь инструментом .querySelector()
+let newTextName = document.querySelector('.popup__input_type_name')
+let newTextJob = document.querySelector('.popup__input_type_job')
+
+const popupCard = document.querySelector('.popup_add_card');
+const openPopupCard = document.querySelector('.profile__button-add');
+const closePopupCard = popupCard.querySelector('.popup__close-button_add_card');
+
+
+const togglePopup = () => {
+    popup.classList.toggle('popup_opened');
+}
+
+
+const togglePopupCard = () => {
+    popupCard.classList.toggle('popup_opened');
+}
+
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+    nameInput.textContent = newTextName.value;
+    jobInput.textContent = newTextJob.value;
+    togglePopup();
+}
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+
+
+openPopupButton.addEventListener('click', () => {
+    togglePopup();
+    newTextName.value = nameInput.textContent;
+    newTextJob.value = jobInput.textContent;
+})
+closePopupButton.addEventListener('click', togglePopup);
+
+
+openPopupCard.addEventListener('click', togglePopupCard)
+closePopupCard.addEventListener('click', togglePopupCard);
+
+
+
+
+
+
+// let cardsContainer = document.querySelector('.cards');
+// const cardElement = document.querySelector('.card');
+
+// function addcard(imageValue, titleValue) {
+//     const cardTemplate = document.querySelector('#card-template').content;
+//     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+//     cardElement.querySelector('.card__image').textContent = imageValue;
+//     cardElement.querySelector('.card__name').textContent = titleValue;
+    
+//     cardElement.querySelector('.card__like').addEventListener('click', (e) => {
+//     e.target.classList.toggle('card__like_active');
+//     });
+    
+//     cardsContainer.append(cardElement);
+//   }
+
+
+
+
+// let cardsContainer = document.querySelector('.cards');
+// function addcard(newNameCard, newCardImage) {
+//     const cardTemplate = document.querySelector('#card-template').content;
+//     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+//     nameInputCard.textContent = newNameCard.value;
+//     imageInputCard.src = newCardImage.value;
+//     imageInputCard.alt = newNameCard.value;
+
+    
+//     cardElement.querySelector('.card__like').addEventListener('click', (e) => {
+//     e.target.classList.toggle('card__like_active');
+//     });
+    
+//     cardsContainer.append(cardElement);
 // }
 
-// likes.addEventListener('click', (e) => {
-// debugger;    toggleClass(e.currentTarget);
+
+//  initialCards.forEach(function(e) {
+//     const template = document.querySelector('#card').textContent;
+//     const card = card.cloneNode(true);
+//     nameInputCard.textContent = element.name;
+//     imageInputCard.src = element.link;
+//     imageInputCard.alt = element.name;
+//     list.appen(card)
 // })
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+// let formElementCard = document.querySelector('.popup__form_add_card');
+// let nameInputCard = document.querySelector('.card__name');
+// let imageInputCard = document.querySelector('.card__image');
+// let newNameCard = document.querySelector('.popup__input_card_name')
+// let newCardImage = document.querySelector('.popup__input_card_image')
+
+// function formSubmitHandlerCard (evt) {
+//     evt.preventDefault();
+
+//     nameInputCard.textContent = newNameCard.value;
+//     imageInputCard.src = newCardImage.value;
+//     imageInputCard.alt = newNameCard.value;
+
+//         togglePopupCard();
+//     }
+// formElementCard.addEventListener('submit', formSubmitHandlerCard); 
+
+const items = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const list = document.querySelector(".cards");
+const itemTemplate = document.querySelector(".card-template").content;
+const formButton = document.querySelector(".popup__button-save"); // кнопка сабмита
+const formInput = document.querySelector(".popup__input_card_name"); // инпут формы
+const formInputImg = document.querySelector(".popup__input_card_image"); // инпут формы
+
+
+
+function render() {
+	items.forEach(renderItem);
+}
+
+// основная функция рендеринга
+function renderItem(text) {
+	const newHtmlElement = itemTemplate.cloneNode(true); // клонируем ноду
+	const header = newHtmlElement.querySelector('.card__name');
+    const image = newHtmlElement.querySelector('.card__image');
+    header.textContent = text.name; // устанавливаем заголовок элемента
+    image.src = text.link;
+
+	// setListenersForItem(newHtmlElement); // назначаем листенеры внутри каждого элемента
+	list.appendChild(newHtmlElement); // вставляем в DOM
+}
+
+
+
+
+
+
+render();
